a8e68351210c5580122174b7abb833c680c5daac
Jason Dsouza <jason.dsouza@au.harveynorman.com>
2019-09-13 16:08:30 +1000

    added initialize method

--- src/Module/Magento2.php
+++ src/Module/Magento2.php
@@ -2,13 +2,22 @@
 
 namespace Codeception\Module;
 
+use Codeception\Lib\Interfaces\PartedModule;
 use Codeception\Module;
 
 /**
  * 
  */
-class Magento2 extends Module
+class Magento2 extends Module implements PartedModule
 {
-
+    /**
+     * Undocumented function
+     *
+     * @return void
+     */
+    public function _initialize()
+    {
+        
+    }
 }
 ?>
